af3eb3480b10de3792985101a721266adc22aeda
unknown <slee230@uncc.edu>
2024-02-21 01:31:44 -0500

    feat: update 2 functions

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -5,6 +5,17 @@ class SandwichMaker:
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
         #####
+        for ingredient, quantity in ingredients.items():
+            if self.machine_resources.get(ingredient, 0) < quantity:
+                print(f"Sorry, there is not enough {ingredient}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
         ########
+        """Deduct the required ingredients from the resources.
+                   Hint: no output"""
+        for ingredient, quantity in order_ingredients.items():
+            self.machine_resources[ingredient] -= quantity
+
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
