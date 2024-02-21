1331f7854480bd8ab5889f436845b1f6decb3985
unknown <slee230@uncc.edu>
2024-02-21 01:50:41 -0500

    refactor: update main loop with correct instance naming

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -10,7 +10,6 @@ sandwich_maker_instance = SandwichMaker(resources)
 cashier_instance = Cashier()
 
 def main():
-    ###  write the rest of the codes ###
     # Loop to keep machine running
     while True:
         user_input = input("What would you like? (small/medium/large/off/report): ").lower()  # case-insensitive
@@ -22,8 +21,8 @@ def main():
         # Report of current resources
         elif user_input == "report":
             # Listed resources displayed along with their current values
-            for ingredient, quantity in sandwich_machine.machine_resources.items():
-                unit = 'slice(s)' if 'bread' in ingredient or 'ham' in ingredient else 'pound(s)'
+            for ingredient, quantity in sandwich_maker_instance.machine_resources.items():
+                unit = 'slice(s)' if 'bread' in ingredient or 'ham' in ingredient else 'ounce(s)'
                 print(f"{ingredient.capitalize()}: {quantity} {unit}")
         # Process the user's order and inserted coins
         elif user_input in recipes:
@@ -32,14 +31,14 @@ def main():
             order_ingredients = recipes[order_size]["ingredients"]
 
             # Checking whether there are enough resources to make the sandwich
-            if sandwich_machine.check_resources(order_ingredients):
+            if sandwich_maker_instance.check_resources(order_ingredients):
                 print("Please insert coins.")
-                coins_inserted = sandwich_machine.process_coins()
+                coins_inserted = cashier_instance.process_coins()
                 order_cost = recipes[order_size]["cost"]
 
-                # Checking if transaction is successful and if change needs to be distributed
-                if sandwich_machine.transaction_result(coins_inserted, order_cost):
-                    sandwich_machine.make_sandwich(order_size, order_ingredients)
+                # Checking if the transaction is successful and if change needs to be distributed
+                if cashier_instance.transaction_result(coins_inserted, order_cost):
+                    sandwich_maker_instance.make_sandwich(order_size, order_ingredients)
             else:
                 # Invalid input
                 print("")
